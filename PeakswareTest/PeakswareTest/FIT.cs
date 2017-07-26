596c42ab1fc51667bb3ca83c06cadea2b992a983
Bill Best <william.best@jeppesen.com>
2017-07-26 15:11:36 -0600

    Added FIT class and interface

---
+++ PeakswareTest/PeakswareTest/FIT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeakswareTest
+{
+    class FIT : IFIT
+    {
+        public FIT()
+        {
+
+        }
+        public double CalculateAverage ()
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ PeakswareTest/PeakswareTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PeakswareTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fit = new FIT();
+            var average = fit.CalculateAverage();
+            Console.WriteLine("Average = {0:f20}", average);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
